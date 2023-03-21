de6f2b9d1ac7a13656261af619cc7873c0813590
mglruanmota <128146623+mglruanmota@users.noreply.github.com>
2023-03-20 23:03:18 -0300

    Delete cadastro directory

--- cadastro/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', '');
-    define('BASE', 'cadastro');
-     
-     $conn = new MYSQLi(HOST,USER,PASS,BASE); 
-
-?>
